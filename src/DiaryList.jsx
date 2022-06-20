0c1d6a291313ac0952435fb0c514bb7d355de718
leedo97y <dlehdus1433@gmail.com>
2022-06-20 19:29:03 +0900

    6/20 DiaryList 추가

---
+++ src/DiaryList.jsx
@@ -0,0 +1,24 @@
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+  <div className="DiaryList">
+    <h2>일기 리스트</h2>
+    <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+    <div>
+      {diaryList.map((it) => (
+        <DiaryItem key={it.id} {...it}/>
+      ))}
+    </div>
+  </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList : [],
+};
+// props가 undefined 일 때 기본값이 필요
+// diaryList의 값을 빈 배열로 설정하여 오류를 없앨 수 있다.
+
+export default DiaryList;
